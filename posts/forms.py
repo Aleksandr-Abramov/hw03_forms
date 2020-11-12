109a86b05e3fefed259cb30124abfd2d81b126be
Alex <ale19850505@gmail.com>
2020-11-12 20:44:25 +0300

    update dz hw03_forms_v.0.2

--- posts/forms.py
+++ posts/forms.py
@@ -1,9 +1,21 @@
 from django import forms
-from .models import Group
+from django.forms import ModelForm
 
-class PostForm(forms.Form):
-    group = forms.ModelChoiceField(queryset=Group.objects.all(),
-                                   label="Группа",
-                                   required=False
-                                   )
-    text = forms.CharField(widget=forms.Textarea)
+from .models import Group
+from .models import Post
+
+#Максим, здравствуйте. Сохранил старый вариант создания формы в комментариях. Как напоминание. Прошу, не осуждать.)
+# class PostForm(forms.Form):
+#     group = forms.ModelChoiceField(queryset=Group.objects.all(),
+#                                    label="Группа",
+#                                    required=False
+#                                    )
+#     text = forms.CharField(widget=forms.Textarea)
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ["group", "text"]
+        widgets = {
+            "text": forms.Textarea()
+        }
--- posts/views.py
+++ posts/views.py
@@ -11,19 +11,15 @@ def index(request):
 
 
 def new_post(request):
+    #Максим, я просто не представляю, как можно написать иначе функцию в концепции "Guard Block".
+    #Статью прочел, буду следовать. Но, в этом случае...
     if request.method == "POST":
-
         form = PostForm(request.POST)
         if form.is_valid():
-            group = form.cleaned_data["group"]
-            text = form.cleaned_data["text"]
-            author = request.user
-            Post.objects.create(group=group,
-                                text=text,
-                                author=author
-                                )
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
             return redirect("index")
-
         return render(request, 'index.html', {'form': form})
 
     form = PostForm()
@@ -34,3 +30,24 @@ def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.all()[:12]
     return render(request, "group.html", {"group": group, "posts": posts})
+
+#Максим, здравствуйте. Сохранил старый вариант создания формы в комментариях. Как напоминание. Прошу, не осуждать.)
+
+# def new_post(request):
+#     if request.method == "POST":
+#
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             group = form.cleaned_data["group"]
+#             text = form.cleaned_data["text"]
+#             author = request.user
+#             Post.objects.create(group=group,
+#                                 text=text,
+#                                 author=author
+#                                 )
+#             return redirect("index")
+#
+#         return render(request, 'index.html', {'form': form})
+#
+#     form = PostForm()
+#     return render(request, "new.html", {"form": form})
